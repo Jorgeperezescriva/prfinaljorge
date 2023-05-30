79420f1e3c23495fec73514a87e16685e9420cf4
jorge <jorge.perezescriva@gmail.com>
2023-05-30 19:05:56 +0200

    terraform

--- main.tf
+++ main.tf
@@ -1,35 +1,37 @@
-terraform {
-  required_version = "~> 1.3"
+# terraform {
+#   required_version = "~> 1.3"
 
-  backend "s3" {
-    bucket         = "nombre-del-bucket-s3"
-    key            = "ruta/del/archivo.tfstate"
-    region         = "eu-west-1"
-    encrypt        = true
-    dynamodb_table = "nombre-de-la-tabla-dynamodb"
-  }
+#   backend "s3" {
+#     bucket         = "nombre-del-bucket-s3"
+#     key            = "ruta/del/archivo.tfstate"
+#     region         = "eu-west-1"
+#     encrypt        = true
+#     dynamodb_table = "nombre-de-la-tabla-dynamodb"
+#   }
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-/* 
-terraform {
-  required_version = "~> 1.3"
-}
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
+# The configuration for the `remote` backend.
+     terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "prfinaljorge"
 
-provider "local" {
-  # Configuración específica del proveedor local, si es necesario
-}
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "prfinlajorge"
+         }
+       }
+     }
 
-# Aquí puedes definir los recursos y configuraciones para tu entorno local
-# Utiliza los bloques de recursos y configuraciones adecuados según tus necesidades
-
-resource "local_file" "ejemplo" {
-  filename = "ruta/al/archivo.txt"
-  content  = "Contenido de ejemplo"
-}
- */
+     # An example resource that does nothing.
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }
